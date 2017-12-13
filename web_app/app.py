a137800158af32d238416fe88995a9c1994d4bb0
Alex Hendra <alexhendra_panjaitan@yahoo.com>
2017-12-13 15:54:43 +0700

    Penambahan Alchemy

--- web_app/app.py
+++ web_app/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String
 
 
 def create_app():
@@ -10,10 +12,23 @@ def create_app():
 
     # config diambil dari python file settings.py
     app.config.from_pyfile('settings.py')
+    db = SQLAlchemy(app)
+
+    class Page(db.Model):
+        __tablename__ = 'page'
+        id = Column(Integer, primary_key=True)
+        contents = Column(String)
+
+    db.create_all()
 
     @app.route('/')
     def index():
-        return render_template('index.html', TITLE='Index Learning')
+        return render_template('index.html', TITLE='Index Learning', CONTENT='''
+            <div class="starter-template">
+                <h1>Welcome to my page !!</h1>
+                <p class="lead">Learning by Doing !!!</p>
+            </div>
+        ''')
 
     @app.route('/about')
     def about():
@@ -21,16 +36,18 @@ def create_app():
 
     @app.route('/testdb')
     def testdb():
-        import psycopg2
+        #import psycopg2
 
-        con = psycopg2.connect('dbname=flask01 user=devuser password=devpassword host=postgres')
-        cur = con.cursor()
-        cur.execute('select * from page;')
+        #con = psycopg2.connect('dbname=flask01 user=devuser password=devpassword host=postgres')
+        #cur = con.cursor()
+        #cur.execute('select * from page;')
 
-        id, title = cur.fetchone()
-        con.close()
+        #id, title = cur.fetchone()
+        #con.close()
+        #return 'ID:{}, Title:{}'.format(id, title)
 
-        return 'ID:{}, Title:{}'.format(id, title)
+        page = Page.query.filter_by(id=1).first()
+        return render_template('index.html', TITLE='Simply Blog', CONTENT=page.contents)
 
     return app
 
